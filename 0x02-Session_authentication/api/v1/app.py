6a4c90d101e944a2e74ea02ba0ce3b2daeb62f84
vrxmike <alvinvrxmike@icloud.com>
2023-08-11 00:33:19 +0000

    fix app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,11 +43,11 @@ def bef_req():
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/apv1/forbidden/',
+            '/api/v1/forbidden/',
             '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            cookie = auth.session_cookie(rquest)
+            cookie = auth.session_cookie(request)
             if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
